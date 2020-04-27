1ca33c563c5c6bbcb06c44d419b0350c69589b8f
Clay Herendeen <clay@marketchronologix.com>
2020-04-27 08:50:35 -0400

    Updated web_driver_provider to load using webdriver_manager

--- web/web_driver_provider.py
+++ web/web_driver_provider.py
@@ -1,16 +1,12 @@
 import atexit
-import os
-import sys
-from shutil import which
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 
 
 class WebDriverProvider:
 
     @staticmethod
     def get_docker_web_driver():
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
 
         # Running in docker container on linux
         # chrome driver install location
@@ -36,34 +32,22 @@ class WebDriverProvider:
 
     @staticmethod
     def get_web_driver(use_headless_web_driver=False):
+        from selenium import webdriver
+        from webdriver_manager.chrome import ChromeDriverManager
 
-        options = webdriver.ChromeOptions()
+        chrome_driver_binary_path = ChromeDriverManager().install()
 
-        if sys.platform == 'darwin':
-            # MacOS
-            if os.path.exists("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"):
-                options.binary_location = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-            elif os.path.exists("/Applications/Chrome.app/Contents/MacOS/Google Chrome"):
-                options.binary_location = "/Applications/Chrome.app/Contents/MacOS/Google Chrome"
-        elif 'linux' in sys.platform:
-            # Linux
-            options.binary_location = which('google-chrome') or \
-                                      which('chrome') or \
-                                      which('chromium')
-        else:
-            # Windows
-            if os.path.exists('C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'):
-                options.binary_location = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe'
-            elif os.path.exists('C:/Program Files/Google/Chrome/Application/chrome.exe'):
-                options.binary_location = 'C:/Program Files/Google/Chrome/Application/chrome.exe'
-
-        chrome_driver_binary = which('chromedriver') or "/usr/local/bin/chromedriver"
+        chrome_options = webdriver.ChromeOptions()
 
         if use_headless_web_driver:
-            options.add_argument('--headless')
-            options.add_argument("--no-sandbox")
-            options.add_argument('--disable-gpu')
-            options.add_argument('--disable-dev-shm-usage')
-            options.add_argument("--window-size=1920, 1200")
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument("--no-sandbox")
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument("--window-size=1920, 1200")
 
-        return webdriver.Chrome(chrome_driver_binary, chrome_options=options)
+        driver = webdriver.Chrome(chrome_driver_binary_path, chrome_options=chrome_options)
+
+        atexit.register(lambda: driver.quit())
+
+        return driver
